fc67b2c343953ff9d790ce837fc61c4ba05b951a
migmen <migmen@users.noreply.github.com>
2022-10-13 21:04:01 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-
+basic.showLeds(`
+    # . . # .
+    . . . . .
+    . . . # .
+    . # # . .
+    . . . . .
+    `)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        music.playMelody("F - A D F G E - ", 120)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        music.playMelody("F A E D F - C5 E ", 120)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # .
+            . # . . .
+            . . . . .
+            `)
+    }
+})
